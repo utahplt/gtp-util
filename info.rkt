df24fbd0193d4fb375456a67a91623c0a95881db
Ben Greenman <benjaminlgreenman@gmail.com>
2018-01-31 01:08:35 -0500

    ** fix deps

--- info.rkt
+++ info.rkt
@@ -1,7 +1,7 @@
 #lang info
 (define collection "gtp-util")
-(define deps '("base"))
-(define build-deps '("rackunit-lib" "racket-doc" "scribble-doc" "rackunit-abbrevs"))
+(define deps '("base" "math-lib" "pict-lib"))
+(define build-deps '("rackunit-lib" "racket-doc" "scribble-lib" "scribble-doc" "rackunit-abbrevs"))
 (define pkg-desc "Utility functions")
 (define version "0.1")
 (define pkg-authors '(ben))
